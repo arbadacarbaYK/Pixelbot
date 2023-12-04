5930af85674102368075a1c0809ad017c9d1eba5
arbadacarba <63317640+arbadacarbaYK@users.noreply.github.com>
2023-12-04 21:04:08 +0100

    Update pixelateTG.py

--- pixelateTG.py
+++ pixelateTG.py
@@ -41,7 +41,19 @@ def button_click(update: Update, context: CallbackContext):
             ))
 
 def process_image(photo_path, chat_id, file_id, bot):
-    # Your pixelation logic here
+    image = cv2.imread(photo_path)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    faces = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml').detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
+
+    for (x, y, w, h) in faces:
+        face = image[y:y+h, x:x+w]
+        pixelated_face = cv2.resize(face, (0, 0), fx=0.03, fy=0.03, interpolation=cv2.INTER_NEAREST)
+        image[y:y+h, x:x+w] = cv2.resize(pixelated_face, (w, h), interpolation=cv2.INTER_NEAREST)
+
+    processed_path = f"processed/{chat_id}_{file_id}_pixelated.jpg"
+    cv2.imwrite(processed_path, image, [int(cv2.IMWRITE_JPEG_QUALITY), 95])
+
+    return processed_path
 
 # fmt: off
 def liotta_overlay(photo_path, chat_id, bot):
