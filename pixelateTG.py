c83742979e7fc9b463be90edf2d1dbaa254d036b
arbadacarba <63317640+arbadacarbaYK@users.noreply.github.com>
2023-12-04 21:04:40 +0100

    Update pixelateTG.py

--- pixelateTG.py
+++ pixelateTG.py
@@ -1,77 +1,87 @@
 import os
 import cv2
+import numpy as np
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import Updater, MessageHandler, Filters, CallbackContext, CallbackQueryHandler
+from telegram.ext import Updater, MessageHandler, Filters, CallbackContext, CommandHandler, CallbackQueryHandler
 from concurrent.futures import ThreadPoolExecutor
 from mtcnn.mtcnn import MTCNN
 
 TOKEN = os.environ['TELEGRAM_BOT_TOKEN']
 MAX_THREADS = 5
 
-def start(update: Update, context: CallbackContext):
-    keyboard = [
-        [InlineKeyboardButton("Pixelate Faces", callback_data='pixelate')],
-        [InlineKeyboardButton("Liotta Overlay", callback_data='liotta')],
-    ]
-    reply_markup = InlineKeyboardMarkup(keyboard)
-    update.message.reply_text('Send me a picture, and choose an option:', reply_markup=reply_markup)
+def start(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text('Send me a picture, and I will pixelate faces in it!')
 
-def button_click(update: Update, context: CallbackContext):
-    query = update.callback_query
-    option = query.data
-    file_id = query.message.photo[-1].file_id
+def pixelate_faces(update: Update, context: CallbackContext) -> None:
+    file_id = update.message.photo[-1].file_id
     file = context.bot.get_file(file_id)
+    
+    # Extract the file name from the file path
     file_name = file.file_path.split('/')[-1]
+    
+    # Construct the local file path
     photo_path = f"downloads/{file_name}"
     file.download(photo_path)
 
-    if option == 'pixelate':
-        with ThreadPoolExecutor(max_workers=MAX_THREADS) as executor:
-            future = executor.submit(process_image, photo_path, update.message.chat_id, file_id, context.bot)
-            future.add_done_callback(lambda fut: context.bot.send_photo(
-                chat_id=update.message.chat_id,
-                photo=open(fut.result(), 'rb')
-            ))
-    elif option == 'liotta':
-        with ThreadPoolExecutor(max_workers=MAX_THREADS) as executor:
-            future = executor.submit(liotta_overlay, photo_path, update.message.chat_id, context.bot)
-            future.add_done_callback(lambda fut: context.bot.send_photo(
-                chat_id=update.message.chat_id,
-                photo=open(fut.result(), 'rb')
-            ))
+    keyboard = [
+        [InlineKeyboardButton("Pixelate", callback_data='pixelate')],
+        [InlineKeyboardButton("Liotta Overlay", callback_data='liotta')],
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    update.message.reply_text('Choose an option:', reply_markup=reply_markup)
 
-def process_image(photo_path, chat_id, file_id, bot):
+    # Save photo_path and user_id in context for later use in button callback
+    context.user_data['photo_path'] = photo_path
+    context.user_data['user_id'] = update.message.from_user.id
+
+def button_callback(update: Update, context: CallbackContext) -> None:
+    query = update.callback_query
+    query.answer()
+
+    option = query.data
+    photo_path = context.user_data['photo_path']
+    user_id = context.user_data['user_id']
+
+    if option == 'pixelate':
+        processed_path = process_image(photo_path, user_id, 'pixelated', context.bot)
+    elif option == 'liotta':
+        processed_path = liotta_overlay(photo_path, user_id, context.bot)
+
+    context.bot.send_photo(chat_id=update.callback_query.message.chat_id, photo=open(processed_path, 'rb'))
+
+def process_image(photo_path, user_id, file_id, bot):
     image = cv2.imread(photo_path)
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    faces = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml').detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
+    faces = detect_faces(image)
 
     for (x, y, w, h) in faces:
         face = image[y:y+h, x:x+w]
         pixelated_face = cv2.resize(face, (0, 0), fx=0.03, fy=0.03, interpolation=cv2.INTER_NEAREST)
         image[y:y+h, x:x+w] = cv2.resize(pixelated_face, (w, h), interpolation=cv2.INTER_NEAREST)
 
-    processed_path = f"processed/{chat_id}_{file_id}_pixelated.jpg"
+    processed_path = f"processed/{user_id}_{file_id}.jpg"
     cv2.imwrite(processed_path, image, [int(cv2.IMWRITE_JPEG_QUALITY), 95])
 
     return processed_path
 
-# fmt: off
-def liotta_overlay(photo_path, chat_id, bot):
+def liotta_overlay(photo_path, user_id, bot):
     image = cv2.imread(photo_path)
-    liotta = cv2.imread('liotta.png')
+    liotta = cv2.imread('liotta.png', -1)
 
-    # Resize Liotta to fit the detected face
     faces = detect_faces(image)
-    if faces:
-        (x, y, w, h) = faces[0]  # Assuming only one face is detected
-        liotta_resized = cv2.resize(liotta, (w, h))
-        image[y:y+h, x:x+w] = liotta_resized
 
-    processed_path = f"processed/{chat_id}_liotta.jpg"
+    for (x, y, w, h) in faces:
+        liotta_resized = cv2.resize(liotta, (w, h), interpolation=cv2.INTER_AREA)
+        alpha_s = liotta_resized[:, :, 3] / 255.0
+        alpha_l = 1.0 - alpha_s
+        for c in range(0, 3):
+            image[y:y+h, x:x+w, c] = (alpha_s * liotta_resized[:, :, c] +
+                                       alpha_l * image[y:y+h, x:x+w, c])
+
+    processed_path = f"processed/{user_id}_liotta.jpg"
     cv2.imwrite(processed_path, image, [int(cv2.IMWRITE_JPEG_QUALITY), 95])
 
     return processed_path
-# fmt: on
 
 def detect_faces(image):
     mtcnn = MTCNN()
@@ -79,14 +89,17 @@ def detect_faces(image):
     bounding_boxes = [face['box'] for face in faces]
     return bounding_boxes
 
-def main():
+def main() -> None:
     updater = Updater(TOKEN, use_context=True)
+
     dp = updater.dispatcher
 
-    dp.add_handler(MessageHandler(Filters.photo, start))
-    dp.add_handler(CallbackQueryHandler(button_click))
+    dp.add_handler(MessageHandler(Filters.photo, pixelate_faces))
+    dp.add_handler(CommandHandler('start', start))
+    dp.add_handler(CallbackQueryHandler(button_callback))
 
     updater.start_polling()
+
     updater.idle()
 
 if __name__ == '__main__':
