8c77cfc9a3fbd793ae9182b57a4f52c8e057d32b
arbadacarba <63317640+arbadacarbaYK@users.noreply.github.com>
2023-12-04 19:06:52 +0100

    Update pixelateTG.py

--- pixelateTG.py
+++ pixelateTG.py
@@ -25,7 +25,7 @@ def pixelate_faces(update: Update, context: CallbackContext):
         file.download(photo_path)
 
         with ThreadPoolExecutor(max_workers=MAX_THREADS) as executor:
-            future = executor.submit(process_image, photo_path, update.message.chat_id, context.bot)
+            future = executor.submit(process_image, photo_path, update.message.chat_id, file_id, context.bot)
             future.add_done_callback(lambda fut: context.bot.send_photo(
                 chat_id=update.message.chat_id,
                 photo=open(fut.result(), 'rb')
@@ -33,7 +33,7 @@ def pixelate_faces(update: Update, context: CallbackContext):
     else:
         update.message.reply_text('Please send a photo.')
 
-def process_image(photo_path, chat_id, bot):
+def process_image(photo_path, chat_id, file_id, bot):
     image = cv2.imread(photo_path)
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     faces = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml').detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
@@ -43,7 +43,7 @@ def process_image(photo_path, chat_id, bot):
         pixelated_face = cv2.resize(face, (0, 0), fx=0.03, fy=0.03, interpolation=cv2.INTER_NEAREST)
         image[y:y+h, x:x+w] = cv2.resize(pixelated_face, (w, h), interpolation=cv2.INTER_NEAREST)
 
-    processed_path = f"processed/{chat_id}_pixelated.jpg"
+    processed_path = f"processed/{chat_id}_{file_id}_pixelated.jpg"
     cv2.imwrite(processed_path, image, [int(cv2.IMWRITE_JPEG_QUALITY), 95])
 
     return processed_path
