a07864fff2acd5229917ef74230008e630e78072
arbadacarba <63317640+arbadacarbaYK@users.noreply.github.com>
2023-12-04 15:38:14 +0100

    Create pixelateTG.py

---
+++ pixelateTG.py
@@ -0,0 +1,53 @@
+import os
+import cv2
+import numpy as np
+from telegram import Update
+from telegram.ext import Updater, MessageHandler, Filters, CallbackContext
+from concurrent.futures import ThreadPoolExecutor
+
+TOKEN = os.getenv('TELEGRAM_BOT_TOKEN', 'YOUR_DEFAULT_TOKEN')
+MAX_THREADS = 5
+
+def start(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text('Send me a picture, and I will pixelate faces in it!')
+
+def pixelate_faces(update: Update, context: CallbackContext) -> None:
+    if update.message.photo:
+        file_id = update.message.photo[-1].file_id
+        file = context.bot.get_file(file_id)
+        photo_path = f"downloads/{file.file_path}"
+        file.download(photo_path)
+
+        with ThreadPoolExecutor(max_workers=MAX_THREADS) as executor:
+            future = executor.submit(process_image, photo_path, update.message.chat_id, context.bot)
+            future.add_done_callback(lambda fut: context.bot.send_photo(
+                chat_id=update.message.chat_id,
+                photo=open(fut.result(), 'rb')
+            ))
+    else:
+        update.message.reply_text('Please send a photo.')
+
+def process_image(photo_path, chat_id, bot):
+    image = cv2.imread(photo_path, cv2.IMREAD_UNCHANGED)
+
+    # ... (rest of the image processing logic remains the same)
+
+    processed_path = f"processed/{chat_id}_{file_path}"
+    cv2.imwrite(processed_path, image, [int(cv2.IMWRITE_JPEG_QUALITY), 95])
+
+    return processed_path
+
+def main() -> None:
+    updater = Updater(TOKEN)
+
+    dp = updater.dispatcher
+
+    dp.add_handler(MessageHandler(Filters.photo, pixelate_faces))
+    dp.add_handler(MessageHandler(Filters.command & Filters.text & ~Filters.update.edited_message, start))
+
+    updater.start_polling()
+
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
