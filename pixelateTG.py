355fcf68228765c0d0388a4f2e6d28e39499c709
arbadacarba <63317640+arbadacarbaYK@users.noreply.github.com>
2023-12-04 20:17:37 +0100

    Update pixelateTG.py

--- pixelateTG.py
+++ pixelateTG.py
@@ -2,6 +2,7 @@
 
 import os
 import cv2
+import numpy as np
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import Updater, MessageHandler, Filters, CallbackContext, CallbackQueryHandler
 from concurrent.futures import ThreadPoolExecutor
@@ -50,8 +51,25 @@ def pixelate_faces(update: Update, context: CallbackContext):
         update.message.reply_text('Please send a photo.')
 
 def overlay_faces(update: Update, context: CallbackContext):
-    # Implement the logic to overlay faces with Liotta image
-    pass
+    if update.message.photo:
+        file_id = update.message.photo[-1].file_id
+        file = context.bot.get_file(file_id)
+
+        # Extract the file name from the file path
+        file_name = file.file_path.split('/')[-1]
+
+        # Construct the local file path
+        photo_path = f"downloads/{file_name}"
+        file.download(photo_path)
+
+        with ThreadPoolExecutor(max_workers=MAX_THREADS) as executor:
+            future = executor.submit(process_image_with_liotta, photo_path, update.message.chat_id, file_id, context.bot)
+            future.add_done_callback(lambda fut: context.bot.send_photo(
+                chat_id=update.message.chat_id,
+                photo=open(fut.result(), 'rb')
+            ))
+    else:
+        update.message.reply_text('Please send a photo.')
 
 def process_image(photo_path, chat_id, file_id, bot):
     image = cv2.imread(photo_path)
@@ -66,6 +84,31 @@ def process_image(photo_path, chat_id, file_id, bot):
         image[y:y + h, x:x + w] = cv2.resize(pixelated_face, (w, h), interpolation=cv2.INTER_NEAREST)
 
     processed_path = f"processed/{chat_id}_{file_id}_pixelated.jpg"
+    cv2.imwrite(processed_path, image, [int(cv2.IMWRITE_JPEG_QUALITY), 95])
+
+    return processed_path
+
+def process_image_with_liotta(photo_path, chat_id, file_id, bot):
+    image = cv2.imread(photo_path)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    # Use the more lightweight MTCNN model for face detection
+    faces = detect_faces(image)
+
+    liotta_path = "liotta.png"
+    liotta = cv2.imread(liotta_path, cv2.IMREAD_UNCHANGED)
+
+    for (x, y, w, h) in faces:
+        liotta_resized = cv2.resize(liotta, (w, h), interpolation=cv2.INTER_AREA)
+
+        alpha_liotta = liotta_resized[:, :, 3] / 255.0
+        alpha_image = 1.0 - alpha_liotta
+
+        for c in range(0, 3):
+            image[y:y + h, x:x + w, c] = (alpha_liotta * liotta_resized[:, :, c] +
+                                           alpha_image * image[y:y + h, x:x + w, c])
+
+    processed_path = f"processed/{chat_id}_{file_id}_with_liotta.jpg"
     cv2.imwrite(processed_path, image, [int(cv2.IMWRITE_JPEG_QUALITY), 95])
 
     return processed_path
@@ -90,3 +133,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+
