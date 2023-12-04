942a4dc553c648efc172617a21aea8458d4f3d6b
arbadacarba <63317640+arbadacarbaYK@users.noreply.github.com>
2023-12-04 19:49:36 +0100

    Update pixelateTG.py

--- pixelateTG.py
+++ pixelateTG.py
@@ -5,6 +5,7 @@ import cv2
 from telegram import Update
 from telegram.ext import Updater, MessageHandler, Filters, CallbackContext
 from concurrent.futures import ThreadPoolExecutor
+from mtcnn.mtcnn import MTCNN
 
 TOKEN = os.environ['TELEGRAM_BOT_TOKEN']
 MAX_THREADS = 5
@@ -16,10 +17,10 @@ def pixelate_faces(update: Update, context: CallbackContext):
     if update.message.photo:
         file_id = update.message.photo[-1].file_id
         file = context.bot.get_file(file_id)
-        
+
         # Extract the file name from the file path
         file_name = file.file_path.split('/')[-1]
-        
+
         # Construct the local file path
         photo_path = f"downloads/{file_name}"
         file.download(photo_path)
@@ -36,14 +37,14 @@ def pixelate_faces(update: Update, context: CallbackContext):
 def process_image(photo_path, chat_id, file_id, bot):
     image = cv2.imread(photo_path)
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    
-    # Use a more lightweight face detection model like MTCNN
+
+    # Use the more lightweight MTCNN model for face detection
     faces = detect_faces(image)
-    
+
     for (x, y, w, h) in faces:
-        face = image[y:y+h, x:x+w]
+        face = image[y:y + h, x:x + w]
         pixelated_face = cv2.resize(face, (0, 0), fx=0.03, fy=0.03, interpolation=cv2.INTER_NEAREST)
-        image[y:y+h, x:x+w] = cv2.resize(pixelated_face, (w, h), interpolation=cv2.INTER_NEAREST)
+        image[y:y + h, x:x + w] = cv2.resize(pixelated_face, (w, h), interpolation=cv2.INTER_NEAREST)
 
     processed_path = f"processed/{chat_id}_{file_id}_pixelated.jpg"
     cv2.imwrite(processed_path, image, [int(cv2.IMWRITE_JPEG_QUALITY), 95])
