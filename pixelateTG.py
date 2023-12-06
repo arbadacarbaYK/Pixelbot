c9af7c2d27ed764c81df4bba6880748d94258319
arbadacarba <63317640+arbadacarbaYK@users.noreply.github.com>
2023-12-06 21:20:48 +0100

    Update pixelateTG.py

--- pixelateTG.py
+++ pixelateTG.py
@@ -92,42 +92,10 @@ def skull_overlay(photo_path, user_id, bot):
     image = cv2.imread(photo_path)
     skull = cv2.imread('skullofsatoshi.png', cv2.IMREAD_UNCHANGED)
 
-    heads = detect_heads(image)
+    # Resize Skull of Satoshi with the updated resize factor
+    skull_resized = cv2.resize(skull, (int(SKULL_RESIZE_FACTOR * skull.shape[1]), int(SKULL_RESIZE_FACTOR * skull.shape[0])), interpolation=cv2.INTER_AREA)
 
-    def process_face(x, y, w, h):
-        print(f"Processing head at ({x}, {y}), width: {w}, height: {h}")
-
-        # Similar processing logic as in liotta_overlay function
-        overlay_x = max(0, x - int(0.25 * w))
-        overlay_y = max(0, y - int(0.25 * h))
-
-        roi = image[overlay_y:overlay_y + h, overlay_x:overlay_x + w]
-
-        # Resize Skull of Satoshi with the updated resize factor
-        skull_resized = cv2.resize(skull, (int(SKULL_RESIZE_FACTOR * w), int(SKULL_RESIZE_FACTOR * h)), interpolation=cv2.INTER_AREA)
-
-        print(f"Original skull size: {skull.shape}")
-        print(f"Resized skull size: {skull_resized.shape}")
-
-        alpha_channel = skull_resized[:, :, 3] / 255.0
-
-        mask = cv2.resize(alpha_channel, (w, h), interpolation=cv2.INTER_AREA)
-        mask_inv = 1.0 - mask
-
-        for c in range(0, 3):
-            roi[:, :, c] = (mask * skull_resized[:, :, c] +
-                            mask_inv * roi[:, :, c])
-
-        image[overlay_y:overlay_y + h, overlay_x:overlay_x + w] = roi
-
-    futures = [executor.submit(process_face, x, y, w, h) for (x, y, w, h) in heads]
-    wait(futures)
-
-    processed_path = f"processed/{user_id}_skull_of_satoshi.jpg"
-    cv2.imwrite(processed_path, image, [int(cv2.IMWRITE_JPEG_QUALITY), 95])
-
-    return processed_path
-
+    cv2.imwrite(f"skull_resized_{user_id}.png", skull_resized)
 
 def button_callback(update: Update, context: CallbackContext) -> None:
     query = update.callback_query
