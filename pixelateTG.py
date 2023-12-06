0f9324cf4faaa6691e461474315bd5bf482078d2
arbadacarba <63317640+arbadacarbaYK@users.noreply.github.com>
2023-12-06 19:54:52 +0100

    Update pixelateTG.py

--- pixelateTG.py
+++ pixelateTG.py
@@ -83,49 +83,36 @@ def liotta_overlay(photo_path, user_id, bot):
 
 def replace_eyes(photo_path, user_id, bot):
     image = cv2.imread(photo_path)
-    lasereye = cv2.imread(LASER_EYE_PATH, cv2.IMREAD_UNCHANGED)
-
-    faces = detect_faces(image)
+    eyes = detect_eyes(image)
 
     def process_eye(eye):
-        if isinstance(eye, dict) and 'box' in eye:  # Check if the output contains a 'box' key
-            box = eye['box']
-            print(f"Processing eye at ({box[0]}, {box[1]}), width: {box[2]}, height: {box[3]}")
+        if 'box' in eye:  # Check if the output contains a 'box' key
+            eye_x, eye_y, eye_w, eye_h = eye['box']
+            # Extract the eye
+            eye_image = image[eye_y:eye_y + eye_h, eye_x:eye_x + eye_w]
 
-            # Adjusting starting position for better alignment
-            overlay_x = max(0, box[0] - int(0.25 * box[2]))
-            overlay_y = max(0, box[1] - int(0.25 * box[3]))
-
-            # Region of interest (ROI) in the original image
-            roi = image[overlay_y:overlay_y + box[3], overlay_x:overlay_x + box[2]]
-
-            # Resize lasereye to match the width and height of the ROI
-            lasereye_resized = cv2.resize(lasereye, (roi.shape[1], roi.shape[0]), interpolation=cv2.INTER_AREA)
+            # Resize lasereye.png to match the width and height of the eye
+            lasereye_resized = cv2.resize(lasereye, (eye_image.shape[1], eye_image.shape[0]), interpolation=cv2.INTER_AREA)
 
             # Extract alpha channel
             alpha_channel = lasereye_resized[:, :, 3] / 255.0
 
-            # Resize mask arrays to match the shape of roi[:, :, c]
-            mask = cv2.resize(alpha_channel, (roi.shape[1], roi.shape[0]), interpolation=cv2.INTER_AREA)
+            # Resize mask arrays to match the shape of eye_image[:, :, c]
+            mask = cv2.resize(alpha_channel, (eye_image.shape[1], eye_image.shape[0]), interpolation=cv2.INTER_AREA)
             mask_inv = 1.0 - mask
 
-            # Blend lasereye and ROI using the resized mask
+            # Blend lasereye.png and eye_image using the resized mask
             for c in range(0, 3):
-                roi[:, :, c] = (mask * lasereye_resized[:, :, c] +
-                                mask_inv * roi[:, :, c])
+                eye_image[:, :, c] = (mask * lasereye_resized[:, :, c] +
+                                      mask_inv * eye_image[:, :, c])
 
-            # Update the original image with the blended ROI
-            image[overlay_y:overlay_y + box[3], overlay_x:overlay_x + box[2]] = roi
+            # Update the original image with the blended eye
+            image[eye_y:eye_y + eye_h, eye_x:eye_x + eye_w] = eye_image
         else:
             print("Invalid eye structure. Skipping.")
 
-    for face in faces:
-        if isinstance(face, dict) and 'keypoints' in face:  # Check if the output contains a 'keypoints' key
-            keypoints = face['keypoints']
-            for eye in keypoints.values():
-                process_eye(eye)
-        else:
-            print("Invalid face structure. Skipping.")
+    futures = [executor.submit(process_eye, eye) for eye in eyes]
+    wait(futures)
 
     processed_path = f"processed/{user_id}_lasereyes.jpg"
     cv2.imwrite(processed_path, image, [int(cv2.IMWRITE_JPEG_QUALITY), 95])
