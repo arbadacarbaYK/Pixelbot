9da6369d77b977a230999b1ea85291afa23e7d0f
arbadacarba <63317640+arbadacarbaYK@users.noreply.github.com>
2023-12-28 22:55:51 +0100

    Update pixelateTG.py

--- pixelateTG.py
+++ pixelateTG.py
@@ -193,6 +193,7 @@ def skull_overlay(photo_path, user_id, bot):
     return processed_path
 
 # Swap face function
+# Swap face function
 def swap_face(update: Update, context: CallbackContext) -> None:
     user_id = update.message.from_user.id
     heads = detect_heads(update.message.photo[-1].file_id)
@@ -203,22 +204,27 @@ def swap_face(update: Update, context: CallbackContext) -> None:
         return
 
     # Assuming user's own picture is a PNG file
-    user_picture = context.bot.get_file(update.message.photo[-1].file_id)
+    user_picture_id = update.message.photo[-1].file_id
+    user_picture = context.bot.get_file(user_picture_id)
 
     if user_picture is None:
         update.message.reply_text("Error: Failed to retrieve the user picture.")
         return
 
-    user_picture_path = f"user_{user_id}_picture.png"
+    # Construct the local file path for the user's picture
+    user_picture_path = f"user_{user_id}_picture.jpg"
     user_picture.download(user_picture_path)
 
-    image = cv2.imread(user_picture_path)
+    # Read the user's picture
+    user_picture_image = cv2.imread(user_picture_path)
 
+    # Process each detected face
     for (x, y, w, h) in heads:
         # Rest of the code for face swap...
 
+    # Save the processed image
     processed_path = f"processed/{user_id}_face_swap.jpg"
-    cv2.imwrite(processed_path, image, [int(cv2.IMWRITE_JPEG_QUALITY), 95])
+    cv2.imwrite(processed_path, user_picture_image, [int(cv2.IMWRITE_JPEG_QUALITY), 95])
 
     # Send the processed image back to the user
     with open(processed_path, 'rb') as photo:
