504fee9e852dd09a1e87f1b3f88282aecfd494bd
arbadacarba <63317640+arbadacarbaYK@users.noreply.github.com>
2024-05-26 00:58:54 +0200

    Update pixelateTG.py

--- pixelateTG.py
+++ pixelateTG.py
@@ -108,13 +108,13 @@ def pixelate_faces(update: Update, context: CallbackContext) -> None:
         return
 
     keyboard = [
-        [InlineKeyboardButton("Pixel", callback_data=f'pixelate_{session_id}')],
-        [InlineKeyboardButton("Liotta", callback_data=f'liotta_{session_id}')],
-        [InlineKeyboardButton("Skull of Satoshi", callback_data=f'skull_of_satoshi_{session_id}')],
-        [InlineKeyboardButton("Cats", callback_data=f'cats_overlay_{session_id}')],
-        [InlineKeyboardButton("Pepe", callback_data=f'pepe_overlay_{session_id}')],
-        [InlineKeyboardButton("Chad", callback_data=f'chad_overlay_{session_id}')],
-        [InlineKeyboardButton("Clowns", callback_data=f'clowns_overlay_{session_id}')],
+        [InlineKeyboardButton("⚔️ Pixel", callback_data=f'pixelate_{session_id}')],
+        [InlineKeyboardButton("😂 Liotta", callback_data=f'liotta_{session_id}')],
+        [InlineKeyboardButton("☠️ Skull of Satoshi", callback_data=f'skull_of_satoshi_{session_id}')],
+        [InlineKeyboardButton("🐈‍⬛ Cats", callback_data=f'cats_overlay_{session_id}')],
+        [InlineKeyboardButton("🐸 Pepe", callback_data=f'pepe_overlay_{session_id}')],
+        [InlineKeyboardButton("🏆 Chad", callback_data=f'chad_overlay_{session_id}')],
+        [InlineKeyboardButton("🤡 Clowns", callback_data=f'clowns_overlay_{session_id}')],
         [InlineKeyboardButton("CANCEL", callback_data=f'cancel_{session_id}')],  # Add Cancel button
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
