f59e26a8c24157cf540152ef9f91b64cbe1af450
arbadacarba <63317640+arbadacarbaYK@users.noreply.github.com>
2024-05-26 01:00:47 +0200

    Update pixelateTG.py

--- pixelateTG.py
+++ pixelateTG.py
@@ -110,7 +110,7 @@ def pixelate_faces(update: Update, context: CallbackContext) -> None:
     keyboard = [
         [InlineKeyboardButton("⚔️ Pixel", callback_data=f'pixelate_{session_id}')],
         [InlineKeyboardButton("😂 Liotta", callback_data=f'liotta_{session_id}')],
-        [InlineKeyboardButton("☠️ Skull of Satoshi", callback_data=f'skull_of_satoshi_{session_id}')],
+        [InlineKeyboardButton("☠️ Skull", callback_data=f'skull_of_satoshi_{session_id}')],
         [InlineKeyboardButton("🐈‍⬛ Cats", callback_data=f'cats_overlay_{session_id}')],
         [InlineKeyboardButton("🐸 Pepe", callback_data=f'pepe_overlay_{session_id}')],
         [InlineKeyboardButton("🏆 Chad", callback_data=f'chad_overlay_{session_id}')],
