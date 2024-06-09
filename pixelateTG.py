6228a9c06ac1702adbe184bb58f3ec31af56279e
arbadacarba <63317640+arbadacarbaYK@users.noreply.github.com>
2024-06-09 14:02:44 +0200

    Update pixelateTG.py

--- pixelateTG.py
+++ pixelateTG.py
@@ -218,16 +218,6 @@ def process_image(photo_path, user_id, session_id, bot):
     cv2.imwrite(processed_path, image, [int(cv2.IMWRITE_JPEG_QUALITY), 95])
     return processed_path
 
-
-def pixelate_command(update: Update, context: CallbackContext) -> None:
-    """Handles the /pixel command to pixelate faces in a photo, GIF, or video. Applicable for both DMs and groups."""
-    if update.message.reply_to_message and (update.message.reply_to_message.photo or update.message.reply_to_message.document):
-        context.args = ['pixelate']
-        pixelate_faces(update, context)
-    else:
-        update.message.reply_text('Please reply to a photo, GIF, or video to pixelate faces.')
-
-
 def button_callback(update: Update, context: CallbackContext) -> None:
     query = update.callback_query
     query.answer()
